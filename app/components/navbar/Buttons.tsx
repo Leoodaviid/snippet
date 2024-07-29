1d7f3641c0e41ad368fdc04736c5c72115ae115a
Leoodaviid <david_ax7@hotmail.com>
2024-07-28 21:09:26 -0300

    feat: components header authentication

---
+++ app/components/navbar/Buttons.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useAuth } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+
+const Buttons = () => {
+  const { userId } = useAuth();
+  return (
+    <div className="max-sm:w-full">
+      {userId ? (
+        <Link
+          href="/my-notes"
+          className="max-sm:w-full bg-blue-500 p-2 px-6 text-sm text-white rounded-md"
+        >
+          <button>Access To The App</button>
+        </Link>
+      ) : (
+        <div className="flex gap-2 max-sm:flex-col max-sm:w-full max-sm:mt-8">
+          <Link href="/sign-in">
+            <button className="max-sm:w-full bg-blue-500 px-6 py-2 text-white rounded-md">
+              Sign-In
+            </button>
+          </Link>
+          <Link href="/sign-up">
+            <button className="text-sm border border-blue-500 text-blue-500 py-2 hover:bg-blue-500 hover:text-white px-6 rounded-md">
+              Sign-Up
+            </button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Buttons;
---
+++ app/components/navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "./Logo";
+import Buttons from "./Buttons";
+
+const Navbar = () => {
+  return (
+    <div className="flex m-5 max-sm:mt-09 mx-8 items-center justify-between max-sm:flex-col">
+      <Logo />
+      <Buttons />
+    </div>
+  );
+};
+
+export default Navbar;
